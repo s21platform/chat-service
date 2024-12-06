736182008246d0b4cb2e72a01d1d2a4c4b1ed839
Daniil <122930943+LoskutovDaniil@users.noreply.github.com>
2024-12-06 16:41:10 +0300

    fix repository.go: func connect and New

--- internal/repository/postgres/repository.go
+++ internal/repository/postgres/repository.go
@@ -4,7 +4,6 @@ import (
 	"chat-service/internal/config"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -13,34 +12,20 @@ type Repository struct {
 	connection *sqlx.DB
 }
 
-func connect(cfg *config.Config) (*Repository, error) {
+func connect(cfg *config.Config) *Repository {
 	conStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
 		cfg.Postgres.User, cfg.Postgres.Password, cfg.Postgres.Database, cfg.Postgres.Host, cfg.Postgres.Port)
 
 	db, err := sqlx.Connect("postgres", conStr)
 	if err != nil {
-		return nil, fmt.Errorf("sql.Open: %w", err)
+		log.Fatalf("failed to connect DB")
 	}
 
-	return &Repository{db}, nil
+	return &Repository{db}
 }
 
-func New(cfg *config.Config) (*Repository, error) {
-	var err error
-
-	var repo *Repository
-
-	for i := 0; i < 5; i++ {
-		repo, err = connect(cfg)
-		if err == nil {
-			return repo, nil
-		}
-
-		log.Println(err)
-		time.Sleep(500 * time.Millisecond)
-	}
-
-	return nil, err
+func New(cfg *config.Config) *Repository {
+	return connect(cfg)
 }
 
 func (r *Repository) Close() {
