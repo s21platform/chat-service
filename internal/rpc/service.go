b34a04bd585709893ae68a2091012ec193790abf
Daniil <122930943+LoskutovDaniil@users.noreply.github.com>
2024-12-11 12:54:42 +0300

    implementation of the Getchat method

--- internal/rpc/service.go
+++ internal/rpc/service.go
@@ -1,9 +1,41 @@
 package rpc
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	chat "github.com/s21platform/chat-proto/chat-proto"
 )
 
 type Server struct {
 	chat.UnimplementedChatServiceServer
+	dbR DbRepo
+}
+
+func New(repo DbRepo) *Server {
+	return &Server{
+		dbR: repo,
+	}
+}
+
+func (s *Server) GetChat(ctx context.Context, in *chat.GetChatIn) (*chat.GetChatOut, error) {
+	data, err := s.dbR.GetChat(in.Uuid)
+	if err != nil {
+		return nil, fmt.Errorf("s.dbR.GetChat: %v", err)
+	}
+
+	out := &chat.GetChatOut{
+		Messages: make([]*chat.Message, len(*data)),
+	}
+
+	for i, message := range *data {
+		out.Messages[i] = &chat.Message{
+			Uuid: message.Uuid.String(),
+			Content: message.Content,
+			SentAt: message.SentAt.Format(time.RFC3339),
+		}
+	}
+
+	return out, nil
 }
