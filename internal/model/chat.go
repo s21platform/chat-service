250c5a5a8ab891f75183021f766732f9d3a1d24d
Daniil <122930943+LoskutovDaniil@users.noreply.github.com>
2025-01-23 20:31:31 +0300

    CHT-23: add EditMessage

--- internal/model/chat.go
+++ internal/model/chat.go
@@ -15,3 +15,25 @@ type Message struct {
 type MessageData struct {
 	Messages []Message
 }
+
+type Chat struct {
+	Uuid            uuid.UUID `db:"uuid"`        // uuid чата
+	LastMessage     string    `db:"content"`     // последнее сообщение в чате
+	ChatName        string    `db:"group_name"`  // наименование чата
+	AvatarLink      string    `db:"avatar_link"` // аватарка чата
+	LastMessageTime time.Time `db:"created_at"`  // время последнего сообщения
+}
+
+type ChatData struct {
+	Chats []Chat
+}
+
+type EditMessageRequest struct {
+	MessageUUID uuid.UUID `db:"id"`      // uuid сообщения
+	Content     string    `db:"content"` // новый текст сообщения
+}
+
+type EditedMessage struct {
+	MessageUUID uuid.UUID `db:"id"`      // uuid измененного сообщения
+	Content     string    `db:"content"` // новый текст сообщения
+}
