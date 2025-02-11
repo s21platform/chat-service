abc9d33136c6a1d2c6b243f5b48d8f0c96b5148d
Daniil <122930943+LoskutovDaniil@users.noreply.github.com>
2025-02-11 18:25:47 +0300

    fix error DeleteMessage

--- internal/service/contract.go
+++ internal/service/contract.go
@@ -1,12 +1,10 @@
 package service
 
 import (
-	"github.com/google/uuid"
 	"github.com/s21platform/chat-service/internal/model"
 )
 
 type DBRepo interface {
 	GetRecentMessages(chatUUID string) (*[]model.Message, error)
-	DeleteMessage(messageUUID uuid.UUID, scope model.DeletionScope) (bool, error)
 	EditMessage(messageID string, newContent string) (*model.EditedMessage, error)
 }
