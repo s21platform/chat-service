b57704a42bd222274fa55d6816612ad71bb2dfc4
mikhail-kalugin <alekseevich.m.k@gmail.com>
2025-08-10 20:33:15 +0300

    update migrations

--- migrations/0004_create_group_chats_user_table.sql
+++ migrations/0004_create_group_chats_user_table.sql
@@ -1,15 +1,29 @@
 -- +goose Up
-CREATE TABLE IF NOT EXISTS group_chats_user
+CREATE TYPE message_type AS ENUM ('text', 'image', 'video', 'file', 'speech', 'circle');
+CREATE TYPE delete_format_type AS ENUM ('self', 'all');
+CREATE TABLE IF NOT EXISTS messages
 (
-    uuid          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    chat_uuid     UUID NOT NULL,
-    user_uuid   UUID NOT NULL,
-    username    TEXT NOT NULL,
-    avatar_link TEXT NOT NULL,
-    joined_at   TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (chat_uuid) REFERENCES group_chats (uuid),
-    CONSTRAINT unique_group_chat_user UNIQUE (chat_uuid, user_uuid)
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    stream_id     UUID NOT NULL,
+    sender_id     UUID NOT NULL,
+    type          message_type,
+    content       TEXT,
+    media         JSONB,
+    root_id       UUID,
+    parent_id     UUID,
+    sent_at       TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP,
+    deleted_at    TIMESTAMP,
+    delete_format delete_format_type,
+    deleted_by    UUID,
+    FOREIGN KEY (stream_id) REFERENCES streams (id),
+    FOREIGN KEY (stream_id, sender_id) REFERENCES stream_members (stream_id, sender_id),
+    FOREIGN KEY (root_id) REFERENCES messages (id),
+    FOREIGN KEY (parent_id) REFERENCES messages (id),
+    FOREIGN KEY (stream_id, deleted_by) REFERENCES stream_members (stream_id, user_id)
 );
 
 -- +goose Down
-DROP TABLE IF EXISTS group_chats_user;
+DROP TABLE IF EXISTS messages;
+DROP TYPE IF EXISTS delete_format_type;
+DROP TYPE IF EXISTS message_type;
